fb39e6dd39d978d73f9713b775e5a30aa5a73e82
Infexo <168571113+Infexo@users.noreply.github.com>
2026-03-27 20:16:18 +0530

    Update FloatingWidget registration to return a promise

--- src/lib/floatingWidget.ts
+++ src/lib/floatingWidget.ts
@@ -11,19 +11,22 @@ export class FloatingWidgetWeb extends WebPlugin implements FloatingWidgetPlugin
   async startWidget(options: { timerText: string; subjectName: string }): Promise<void> {
     console.log('FloatingWidget.startWidget called on web with options:', options);
   }
+
   async stopWidget(): Promise<void> {
     console.log('FloatingWidget.stopWidget called on web');
   }
+
   async checkPermission(): Promise<{ granted: boolean }> {
     return { granted: true };
   }
+
   async requestPermission(): Promise<void> {
     console.log('FloatingWidget.requestPermission called on web');
   }
 }
 
 const FloatingWidget = registerPlugin<FloatingWidgetPlugin>('FloatingWidget', {
-  web: () => new FloatingWidgetWeb(),
+  web: () => Promise.resolve(new FloatingWidgetWeb()),
 });
 
 export default FloatingWidget;
